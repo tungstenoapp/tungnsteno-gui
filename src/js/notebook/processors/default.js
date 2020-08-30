ccfb8ce9cc1a677f897ca3fd14f155aec9abc366
JoseCarlosGarcia95 <hola@josecarlos.me>
2020-08-30 18:37:33 +0200

    Add support for mathJAX.

--- src/js/notebook/processors/default.js
+++ src/js/notebook/processors/default.js
@@ -2,5 +2,9 @@ class DefaultProcessor {
     eval(result, cell) {
         cell.output = result.output;
         cell.output_type = 'default';
+
+        setTimeout(function () {
+            MathJax.Hub.Queue(["Typeset", MathJax.Hub]);
+        }, 100);
     }
 };
